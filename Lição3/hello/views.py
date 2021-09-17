96a49dad06beaaba745b7b970560a7a73b17d94a
Andersonv16 <andersonviana50@gmail.com>
2021-09-16 23:59:17 -0300

    paginas html

--- Lição3/hello/views.py
+++ Lição3/hello/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, World!')
+    return render(request , "hello/index.html")
 
 def anderson(request):
     return HttpResponse('Olá, meu nome é Anderson')
 
 def greet(request, name):
-    return HttpResponse(f'Hello, {name.capitalize()}')
+    return render(request, "hello/greet.html", {
+        "name" : name.capitalize()
+    })
